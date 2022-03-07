115d8b19b4e0aa36e1580d3c37060ad9cd8d46c0
Elencrissy <cristina.furgala@gmail.com>
2022-03-07 12:23:12 +0100

    Modal

--- Modal.js
+++ Modal.js
@@ -1,28 +1,78 @@
-import React, { useEffect, useRef } from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import styles from './modal.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faCircleXmark} from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import ReactDOM from "react-dom";
 
-const Modal = ({ modalStyle, children, show, onClose, backdropStyle }) => {
-    const modalRef = useRef(null);
-    useEffect(() => {
-            if (show) {
-                modalRef.current.classList.add(styles.visible);
-            }
-            else {
-                modalRef.current.classList.remove(styles.visible);
-            }
-        }, [show]);
-    return (
-        <div ref={modalRef} style={backdropStyle} className={`${styles.modalWrapper}`}>
-            <FontAwesomeIcon
-                icon="fa-solid fa-circle-xmark"
-                className={styles.closeIcon}
-                onClick={onClose}
-            />
-            <div style={modalStyle} className={styles.modal}>
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  z-index: 1;
+  padding: 20px;
+  box-sizing: border-box;
+  background-color: rgba(0,0,0,0.75);
+  text-align: center;
+  :before{
+    content: "";
+    display: inline-block;
+    height: 100%;
+    vertical-align: middle;
+    margin-right: -0.05em;
+  }
+`
+
+const ModalContent = styled.div`
+  vertical-align: middle;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 99;
+  max-width: 500px;
+  box-sizing: border-box;
+  width: 90%;
+  background: #fff;
+  padding: 15px 30px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px #000;
+  text-align: left;
+  #closeIcon{
+    position: absolute;
+    top: -12.5px;
+    right: -12.5px;
+    display: block;
+    width: 30px;
+    height: 30px;
+    text-indent: -9999px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-color: white;
+    border-radius: 100%;
+  }
+`
+
+const Modal = ({ children, open, onClose }) => {
+    if(!open) return null
+
+    return ReactDOM.createPortal(
+        <ModalWrapper
+            onClick={onClose}>
+            <ModalContent onClick={(e) => e.stopPropagation()}>
                 {children}
-            </div>
-        </div>
+                <FontAwesomeIcon
+                    icon={faCircleXmark}
+                    onClick={onClose}
+                    id={"closeIcon"}
+                />
+            </ModalContent>
+        </ModalWrapper>,
+        document.getElementById("portal")
     );
 };
 
